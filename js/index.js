b96224cb6edb02d9b089f1901668e159e53e44c5
Zuy Ziendien3254@gmail.com <dien3254@gmail.com>
2022-05-24 21:48:00 +0700

    initial

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var swiperIntroduction = new Swiper('.introduction-slider', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.slider-toggle',
+    clickable: true,
+  },
+})
+
+var swiperService = new Swiper('.service-slider', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.slider-toggle',
+    clickable: true,
+  },
+})
